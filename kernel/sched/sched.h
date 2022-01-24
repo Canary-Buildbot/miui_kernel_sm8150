1f01e924c7c6bf41ad9cd39992e6c8b09a83e9de
stic-server-open <1138705738@qq.com>
2022-01-24 10:17:46 +0800

    kernel: sched-debug: Delete the sysrq_sched_debug function code
    * Change from msm-4.4 kernel
    * Original submission information:
       sched/debug: Make sysrq prints of sched debug data optional
    
       Calls to sysrq_sched_debug_show() can yield rather verbose output
       which contributes to log spew and, under heavy load, may increase
       the chances of a watchdog bark.
    
       Make printing of this data optional with the introduction of a
       new Kconfig, CONFIG_SYSRQ_SCHED_DEBUG.
    
    * Aquarius223: Change Linux 4.14.y op: sdm845 out: sm8150 kernel
    
    * Don't know why, it disappeared on msm4.9
    * Delete it, more at ease
    
    Change-Id: Ie1ca510f1d739d4f065902631cb36fabc4568cd4

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1806,7 +1806,6 @@ static inline int idle_get_state_idx(struct rq *rq)
 
 extern void schedule_idle(void);
 
-extern void sysrq_sched_debug_show(void);
 extern void sched_init_granularity(void);
 extern void update_max_interval(void);
 
